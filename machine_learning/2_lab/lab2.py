6304caf9d3f8c1b9b763c9864e3795772d165422
Lucas Rosa <luc1604@hotmail.com>
2018-10-14 15:54:16 -0300

    machine learning lab 2

--- machine_learning/2_lab/lab2.py
+++ machine_learning/2_lab/lab2.py
@@ -1,13 +1,15 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
+import matplotlib.pyplot as plt
 import numpy as np
 
 iris = load_iris()
 
-results = {}
+results = {
+}
 
-for k in range(3,20,2):
+for k in range(3,50,2):
     knn = KNeighborsClassifier(n_neighbors=k)
     results[str(k)] = {}
 
@@ -17,13 +19,28 @@ for k in range(3,20,2):
         data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size=0.4)
         knn.fit(data_train, target_train)
         scores = np.append(scores, knn.score(data_test, target_test))
-    results[str(k)]['60/40'] = "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+    results[str(k)]['60/40'] = scores
 
     # cross_validation 5 folds
     scores = cross_val_score(knn, iris.data, iris.target, cv=5)
-    results[str(k)]['cross_validation'] = "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+    results[str(k)]['cross_validation'] = scores
+
+x = []
+y_split = []
+y_cross = []
 
 for k in results.keys():
     print("+++++++++++++++++++++ K = "+k+" +++++++++++++++++++++")
+    x.append(k)
     for key, val in results[k].items():
-        print("    "+key+": "+val)
+        if key == "60/40":
+            y_split.append(val.mean())
+        elif key == "cross_validation":
+            y_cross.append(val.mean())
+        print("    "+key+": "+"Accuracy: %0.2f (+/- %0.2f)" % (val.mean(), val.std() * 2))
+
+plt.plot(x, y_split)
+plt.show()
+
+plt.plot(x, y_cross)
+plt.show()
