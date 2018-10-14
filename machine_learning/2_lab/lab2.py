96acb5ce4277c04813bdb82a29cf941aac0f90ca
Lucas Rosa <luc1604@hotmail.com>
2018-10-14 16:37:44 -0300

    nothing

--- machine_learning/2_lab/lab2.py
+++ machine_learning/2_lab/lab2.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
+from scipy.stats import ttest_ind, wilcoxon
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,7 +10,7 @@ iris = load_iris()
 results = {
 }
 
-for k in range(3,50,2):
+for k in range(3,30,2):
     knn = KNeighborsClassifier(n_neighbors=k)
     results[str(k)] = {}
 
@@ -29,6 +30,21 @@ x = []
 y_split = []
 y_cross = []
 
+for a in range(3,30,2):
+    a_scores = np.append(
+        results[str(a)]["60/40"],
+        results[str(a)]["cross_validation"]
+    )
+    for b in range(a+2,30,2):
+        b_scores = np.append(
+            results[str(b)]["60/40"],
+            results[str(b)]["cross_validation"]
+        )
+        print(str(a)+" vs "+str(b)+":")
+        print(ttest_ind(a_scores,b_scores))
+        print(wilcoxon(a_scores,b_scores))
+
+
 for k in results.keys():
     print("+++++++++++++++++++++ K = "+k+" +++++++++++++++++++++")
     x.append(k)
@@ -40,7 +56,7 @@ for k in results.keys():
         print("    "+key+": "+"Accuracy: %0.2f (+/- %0.2f)" % (val.mean(), val.std() * 2))
 
 plt.plot(x, y_split)
-plt.show()
+# plt.show()
 
 plt.plot(x, y_cross)
-plt.show()
+# plt.show()
